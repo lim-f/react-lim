5b27c33bfd13cf38129586b53f3ef5de02237978
chenzhongsheng <chenzhongsheng@bytedance.com>
2024-07-17 09:06:32 +0800

    chore: comment

--- dev/index.ts
+++ dev/index.ts
@@ -7,27 +7,6 @@
 
 import { transformReact } from '../src/index';
 
-// const input = `
-// function App() {
-//   let count = 1;
-//   const inc = () => {
-//     count++;
-//   };
-//   return /* @__PURE__ */ jsxDEV(Fragment, { children: /* @__PURE__ */ jsxDEV("button", { onClick: inc, children: [
-//     "count is ",
-//     count
-//   ] }, void 0, true, {
-//     fileName: "/Users/bytedance/code/tack/react-lim-template/src/App.tsx",
-//     lineNumber: 16,
-//     columnNumber: 7
-//   }, this) }, void 0, false, {
-//     fileName: "/Users/bytedance/code/tack/react-lim-template/src/App.tsx",
-//     lineNumber: 15,
-//     columnNumber: 10
-//   }, this);
-// }
-// `;
-
 const input = `
 function App () {
   const person = {
